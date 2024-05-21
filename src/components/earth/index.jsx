051d54a3d5c16ebcfb736cadb200df21703ca765
vbasoon <vbasoon@gmail.com>
2024-05-21 12:05:05 +0300

    adding Globe's textures

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useLoader } from "@react-three/fiber";
+
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNightMap from "../../assets/textures/8k_earth_nightmap.jpg";
+import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import { TextureLoader } from "three";
+
 
 export default function Earth(props) {
+
+  const [colorMap, normalMap, specularMap, cloudsMap ] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap])
   return (
     <>
       <ambientLight intensity={1}/>
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial color="red"/>
+        <meshPhongMaterial specularMap={specularMap}/>
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
       </mesh>
     </>
   )
